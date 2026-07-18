2f9d5a17f403dbde11298f8b00d76fc45b471356
Nuttividad-BCS <francisnatividad333@gmail.com>
2026-07-18 12:15:57 +0800

    Add File I/O

--- learn-cs4/Program.cs
+++ learn-cs4/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Net;
+
+class Program
 {
     public static void Main(string[] args)
     {
@@ -10,7 +12,8 @@
             1 Add Student
             2 Save
             3 Load
-            4 Exit
+            4 View Students
+            5 Exit
             """);
             Console.WriteLine("Enter Input");
 
@@ -21,19 +24,26 @@
                     switch (cho)
                     {
                         case 1:
+                            Student s = Student.InputDetails();
+                            Student.AddStudent(s);
                             break;
                         case 2:
+                            Student.SaveFile();
                             break;
                         case 3:
+                            Student.LoadFile();
                             break;
                         case 4:
+                            Student.ShowStudents();
+                            break;
+                        case 5:
                             status = false;
                             break;
                     }
                 } 
-                catch (Exception)
+                catch (Exception e)
                 {
-                    
+                    Console.WriteLine($"Returning to Main menu... {e}");
                 }
             }
             else
@@ -51,7 +61,104 @@
     public static void InvalidInput()
     {
         Console.WriteLine("Input is invalid!");
-        Console.ReadLine();
+        Console.ReadKey();
         Console.Clear();
+    }
+
+    public static int IntInput(string? msg)
+    {
+        int input = 0;
+        bool status = true;
+        do
+        {
+            if (msg != null)
+            {
+                Console.WriteLine($"Please Enter {msg}");
+            }
+
+            if(int.TryParse(Console.ReadLine(), out int inp))
+            {
+                input = inp;
+                status = false;
+            }
+            else
+            {
+                InvalidInput();
+            }
+        
+        } while(status);
+
+        return input;
+    }
+
+    public static double DoubleInput(string? msg)
+    {
+        double input = 0;
+        bool status = true;
+        do
+        {
+            if (msg != null)
+            {
+                Console.WriteLine($"Please Enter {msg}");
+            }
+
+            if(double.TryParse(Console.ReadLine(), out double inp))
+            {
+                input = inp;
+                status = false;
+            }
+            else
+            {
+                InvalidInput();
+            }
+        
+        } while(status);
+
+        return input;
+    }
+    
+    public static List<double> GradesInput(string? msg)
+    {
+        List<double> grades = new List<double>();
+        while(grades.Count < 5){
+            try
+            {
+                double g = DoubleInput("Input Grade");
+                grades.Add(g);
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine($"Exception occured! {e}");
+            }
+        }
+        return grades;
+    }
+
+    public static string StringInput(string? msg)
+    {
+        string input ="";
+        bool status = true;
+        do
+        {
+            
+            if (msg != null)
+            {
+                Console.WriteLine($"Please Enter {msg}");
+            }
+
+            string inp = Console.ReadLine();
+            if(inp != "" && inp != null)
+            {
+                input = inp;
+                status = false;
+            }
+            else
+            {
+                InvalidInput();
+            }
+        
+        } while(status);
+
+        return input;
     }
 }
--- learn-cs4/Student.cs
+++ learn-cs4/Student.cs
@@ -1,5 +1,11 @@
+using System.Text.Json;
+using System.IO;
+using System.Security.Cryptography;
+
 class Student
 {
+    private static List<Student> students = new List<Student>();
+
     private string _Name;
     private int _Age;
     private string _Subject;
@@ -20,7 +26,7 @@ class Student
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Error Occured: {e}");
+                printException(e);
             }
         }
     }
@@ -45,7 +51,7 @@ class Student
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"Error Occured: {e}");
+                    printException(e);
                 }
             }
            
@@ -66,7 +72,7 @@ class Student
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Error Occured: {e}");
+                printException(e);
             }
         }
     }
@@ -91,7 +97,7 @@ class Student
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine($"Error Occurred: {e}");
+                    printException(e);
                 }
             }
         }
@@ -110,8 +116,138 @@ class Student
         Grades = grades;
     }
 
-    public void AddStudent()
+    public static Student InputDetails()
     {
+        string name;
+        int age=0;
+        string subject;
+        List<double> grades = new List<double>();
+
+        try
+        {
+            name = Program.StringInput("Name");
+
+            age = Program.IntInput("Age");
+
+            subject = Program.StringInput("Subject");
+
+            grades = Program.GradesInput(null);
+
+            return new Student 
+                (   name, 
+                    age, 
+                    subject, 
+                    grades
+                );
+        }
+        catch(Exception)
+        {
+            Program.InvalidInput();
+            throw;
+        }
+    }
+
+    //Student Adder
+    public static void AddStudent(Student s)
+    {
+        try
+        {
+            Student student = new Student(s.Name, s.Age, s.Subject, s.Grades);
+            students.Add(student);
+        }
+        catch (Exception e)
+        {
+            printException(e);
+        }
+    }
+
+    public static void SaveFile()
+    {
+        try
+        {
+            var options = new JsonSerializerOptions {
+                WriteIndented = true
+            };
+
+            string jsonstring = JsonSerializer.Serialize(students, options);
+
+            File.WriteAllText("students.json",jsonstring);
+       } 
+        catch(Exception e)
+        {
+            printException(e);
+        }
+    }
+
+    public static void LoadFile()
+    {
+            using FileStream filePath = File.OpenRead("students.json");
+
+            if (!File.Exists("students.json"))
+            {
+                Console.WriteLine("Save file does not exist!");
+                return;
+            }
+
+            List<Student> deserialized = JsonSerializer.Deserialize<List<Student>>(filePath) ?? [];
+        try
+        {
+            try
+            {
+                BackupCurrent();
+            } 
+            catch(Exception e)
+            {
+                printException(e);
+                throw;
+            } 
+
+            students.Clear();
+            foreach(Student s in deserialized)
+            {
+                AddStudent(s);
+            }   
         
+        } 
+        catch (Exception e)
+        {
+            printException(e);
+            throw;
+        }
+    }
+
+    public static void ShowStudents()
+    {
+        if(isStudentsEmpty())
+        {
+            Console.WriteLine("No save data loaded/No students in file");
+        }
+
+        else
+        {
+            students.ForEach(s => Console.WriteLine($"""
+            Name: {s._Name}
+            Age: {s._Age}
+            Subject: {s._Subject}
+            Grades: {string.Join(", ",s._Grades)}
+            """));
+        }
+
+    }
+
+    public static void BackupCurrent()
+    {
+
+        File.Copy("students.json","students_backup.json", overwrite: true);
+    }
+    //Empty checker
+    public static bool isStudentsEmpty()
+    {
+        return students.Count == 0 || students == null ? true : false;
+    }
+
+    public static void printException(Exception e)
+    {
+        Console.WriteLine($"Exception Occured: {e}");
     }
 }
